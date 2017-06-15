180148473265e9068207bae02847f9368fbda82c
Spencer Rakoczy <sjr62@pitt.edu>
2017-06-15 13:48:01 -0400

    Added jUnit and basic Class structur

---
+++ Cat.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Cat{
+	String _name;
+	double _price;
+	public Cat rentCat(Customer c){
+
+	}
+	public Cat returnCat(Customer c){
+
+	}
+	public void setName(String n){
+
+	}
+	public void setPrice(double p){
+
+	}
+	public String getName(){
+		return _name;
+	}
+	public Double getPrice(){
+		return _price;
+	}
+}
---
+++ RentACat.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class RentACat{
+	//We want a method to rent a cat, the helper method will be in the cat class
+
+	//We want a method to return a cat, the helper method will also be in the cat class. 
+
+	//we want a method to see a list of cats for rent. Helper method will be in cat class (toString). 
+
+	//Method to quit.
+
+	public static void Main(String[] args){
+		ArrayList<Cat> catsForRent= new ArrayList<Cat>();
+		ArrayList<Cat> catsBeingRented=new ArrayList<Cat>();
+		ArrayList<Customer> customerList=new ArrayList<Customer>();
+
+		
+	}
+
+}
